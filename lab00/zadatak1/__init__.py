ece971b8f9f9e15f2bf8e5a4c59fb2109e0cf54b
mdjukic-nit <91469720+mdjukic-nit@users.noreply.github.com>
2021-09-27 13:41:52 +0200

    Changed to english

--- lab00/zadatak1/__init__.py
+++ lab00/zadatak1/__init__.py
@@ -2,15 +2,15 @@ import check50
 
 @check50.check()
 def exists():
-    """pozdrav.c exists."""
-    check50.exists("pozdrav.c")
+    """hello.c exists."""
+    check50.exists("hello.c")
 
 @check50.check(exists)
 def compiles():
-    """no syntax errors in pozdrav.c."""
-    check50.run("gcc pozdrav.c -opozdrav").exit(0)
+    """no syntax errors in hello.c."""
+    check50.run("gcc hello.c -ohello").exit(0)
 
 @check50.check(compiles)
 def prints():
     """prints 3 messages"""
-    check50.run("./pozdrav").stdout("Pozdrav svete!\nPozdrav svete!\nPozdrav svete!\n").exit(0)
+    check50.run("./hello").stdout("Hello world!\nHello world!\nHello world!\n").exit(0)
